59798bf0c0895e3ebc518caa6905f18d97cb1ca5
Simon Cozens <simon@simon-cozens.org>
2019-02-06 12:23:29 +0000

    Add docs

--- patchcommand.lua
+++ patchcommand.lua
@@ -13,3 +13,18 @@ SILE.patchCommand = function(command, opts)
     end
   end
 end
+
+return { documentation = [[\begin{document}
+
+This package is intended to be used by other package developers. It redefines
+a command, running additional code either both its execution, after it, or
+both:
+
+\begin{verbatim}
+    SILE.patchCommand("chapter", \{
+      before = function() print("I am run before a chapter heading") end,
+      after  = function() print("I am run after a chapter heading") end
+    \})
+\end{verbatim}
+
+\end{document}]] }
